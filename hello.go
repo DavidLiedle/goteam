ef771e72e238bc7e1a3aeb169f3f84ffc3a84d5e
David Liedle <david.liedle@gmail.com>
2015-07-28 12:25:25 -0600

    Playing around with Go
    stringutil.Reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"github.com/macdaddy/stringutil"
+)
 
 func main() {
-  fmt.Printf("Hello, team.\n")
+  //fmt.Printf("Hello, team.\n")
+  fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
